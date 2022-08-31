56f32cea89fdc81efd49338e27112a144b52ec46
Fatma Caglar <caglar.fatmaa@outlook.com>
2022-09-01 01:46:13 +0300

    Add files via upload

---
+++ CiftSayilariBulanProgram/src/CiftSayilariBulanProgram.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CiftSayilariBulanProgram {
+    public static void main(String[] args) {
+        int k;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayi Giriniz : ");
+        k = input.nextInt();
+
+
+        for (int i = 1; i <= k; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
